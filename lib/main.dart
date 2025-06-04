21a596e66061a8c7bb54cad0edb0dadf87d7e157
Dan Mathew Gamale <danmathewgamale@gmail.com>
2025-06-04 18:01:41 +0800

    Refactor main.dart to remove MultiProvider and delete unused login_screen and profile_provider files
    - Simplified `main.dart` by removing the `MultiProvider` setup and directly running `MyApp()`.
    - Deleted the unused `login_screen copy.dart` and `profile_provider.dart` files to clean up the codebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,14 @@
 // lib/main.dart
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'core/theme/app_theme.dart';
 import 'features/auth/presentation/login_screen.dart';
-import 'features/profile/presentation/profile_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SharedPreferences.getInstance();
-  runApp(
-    MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => ProfileProvider())],
-      child: const MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
