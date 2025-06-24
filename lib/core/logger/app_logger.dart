c3ed946af3dadfc7d56cc823de932bab731b4b72
Dan Mathew Gamale <danmathewgamale@gmail.com>
2025-06-24 12:42:36 +0800

    Update AppLogger to increase line length for improved log readability
    - Modified the `lineLength` parameter in `AppLogger` from 120 to 1000 to accommodate longer log messages, enhancing the clarity and detail of logged information.

--- lib/core/logger/app_logger.dart
+++ lib/core/logger/app_logger.dart
@@ -5,7 +5,7 @@ class AppLogger {
     printer: PrettyPrinter(
       methodCount: 0, // Number of method calls to be displayed
       errorMethodCount: 8, // Number of method calls if stacktrace is provided
-      lineLength: 120, // Width of the output
+      lineLength: 1000, // Width of the output
       colors: true, // Colorful log messages
       printEmojis: true, // Print an emoji for each log message
     ),
